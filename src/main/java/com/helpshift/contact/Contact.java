71184def8f21f67088593c09e3494b30970331c6
NAVEEN NAHATA <naveen.nahata@MacBook-Pro-2.local>
2017-03-04 18:26:00 +0530

    Adding basic entities

---
+++ src/main/java/com/helpshift/contact/Contact.java
@@ -0,0 +1,12 @@
+package com.helpshift.contact;
+
+import lombok.Data;
+
+/**
+ * Created by naveen.nahata on 04/03/17.
+ */
+@Data
+public class Contact {
+    private String firstName;
+    private String lastName;
+}
