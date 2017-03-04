1f4f3ce41bc323f17855849af81ba94717166be0
naveennahata <naveen.nahata@MacBook-Pro-2.local>
2017-03-04 22:49:35 +0530

    Adding test cases

--- src/test/java/com/helpshift/contact/test/ContactManagerImplTest.java
+++ src/test/java/com/helpshift/contact/test/ContactManagerImplTest.java
@@ -1,24 +1,56 @@
 package com.helpshift.contact.test;
 
 import com.helpshift.contact.Contact;
-import com.helpshift.contact.ContactManager;
 import com.helpshift.contact.ContactManagerImpl;
 import com.helpshift.trie.Trie;
 import com.helpshift.trie.TrieImpl;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by naveen.nahata on 04/03/17.
  */
 public class ContactManagerImplTest {
 
-    ContactManagerImpl contactManager;
+    private static ContactManagerImpl contactManager;
+
+    public ContactManagerImpl getContactManager(){
+        Trie<Contact> firstNameTrie = new TrieImpl<>();
+        Trie<Contact> lastNameTrie = new TrieImpl<>();
+        return new ContactManagerImpl(firstNameTrie,lastNameTrie);
+
+    }
 
     @BeforeClass
-    public void setUp(){
+    public static void setUp(){
        Trie<Contact> firstNameTrie = new TrieImpl<>();
        Trie<Contact> lastNameTrie = new TrieImpl<>();
         contactManager = new ContactManagerImpl(firstNameTrie,lastNameTrie);
     }
 
+    @Test
+    public void addContactTest(){
+        contactManager.addContact("Chris","Harris");
+        contactManager.addContact("Chris","");
+        try {
+            contactManager.addContact("Chris","Harris");
+            Assert.fail();
+        } catch (Exception e){
+            Assert.assertEquals(e.getMessage(),"contact already exist");
+
+        }
+    }
+
+    @Test
+    public void searchContact(){
+        ContactManagerImpl contactManager1 = getContactManager();
+        contactManager1.addContact("Chris","Harris");
+        contactManager1.addContact("Chris","");
+        List<Contact> arrayList = contactManager1.searchContacts("Ch");
+        Assert.assertEquals(arrayList.size(),2);
+    }
 }
